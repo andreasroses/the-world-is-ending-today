c3cf21539a5ff892c1202a8594b73e2e34456fb9
Andrea Mendez <andreacelestem11@gmail.com>
2024-09-15 00:56:23 -0500

    Town Scene assembling,,,

---
+++ Assets/Scripts/I_Interactable.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface I_Interactable
+{
+    public void Interact(PlayerManager player);
+}
+
+public interface I_DialogueInitiator
+{
+    public void StartDialogue(PlayerManager player);
+}
+
+public interface I_Pickup
+{
+    public void StoreItem(PlayerManager player);
+}
